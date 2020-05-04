5720a2ce4a54745c2fe98a94fde290d5e82f495e
EC2 Default User <ec2-user@ip-172-31-80-131.ec2.internal>
2020-05-04 03:18:42 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+  
+  @if(Auth::check())
+  {{Auth::user()->name}}
 
+  @else
   <div class="center jumbotron">
     <div class="text-center">
       <h1>WITH</h1>
-      {!! link_to_route('signup.get', '新規登録する', [], ['class'=>'btn btn-primary mt-4']) !!}
+      <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+          {!! link_to_route('signup.get', '新規登録する', [], ['class'=>'btn btn-primary btn-block mt-4']) !!}
+          {!! link_to_route('login', 'ログインする', [], ['class'=>'btn btn-success btn-block mt-3']) !!}
+        </div>
+      </div>
     </div>
   </div>
+  
+  @endif
 
 @endsection
